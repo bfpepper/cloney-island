fdb0f54ba730b90a9be5e0898b77c72f271d0d2b
antciccone <aciccone@bu.edu>
2016-12-13 19:15:04 -0700

    add slug method to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ApplicationRecord
+  validates :name, uniqueness: true 
+  validates :slug, uniqueness: true 
+
+  before_validation :generate_slug
+
+  def generate_slug 
+    self.slug = name.parameterize 
+  end 
 end
