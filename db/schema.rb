a812368273f9900a897e456255adcf8360c5f1bf
antciccone <aciccone@bu.edu>
2016-12-13 19:12:21 -0700

    removed coverage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214014420) do
+ActiveRecord::Schema.define(version: 20161214020636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161214014420) do
     t.integer  "goal"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
 
 end
--- spec/features/a_user_can_create_a_project_spec.rb
+++ spec/features/a_user_can_create_a_project_spec.rb
@@ -6,13 +6,15 @@ describe "User created a project" do
     scenario "a user fill in fields and clicks submit" do 
       #need to add signed in user 
       visit new_project_path 
+      
+      name = "Ant Lee"
 
-      fill_in :Name, with: "Ant Lee" 
+      fill_in :Name, with: name 
       fill_in :Description, with: "fun"
       fill_in :Goal, with: 500
       click_button "Create Project!" 
 
-      expect(current_path).to eq(project_path) 
+      expect(current_path).to eq(project_path(Project.find_by(name: "Ant Lee")) 
       expect(page).to have_content("Ant Lee")
       expect(page).to have_content("fun")
       exepct(page).to have_content(500)
