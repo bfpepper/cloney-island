a066e13eb971e2d0154fb47a91a2808ff241d803
antciccone <aciccone@bu.edu>
2016-12-14 15:10:51 -0700

    Add project model validations

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "is valid with all attributes" do
+      project = build(:project)
+
+      expect(project).to be_valid
+    end
+
+    xit "is invalid without a name" do
+      project = build(:project, name: nil)
+
+      expect(project).to be_invalid
+    end
+
+    it "is invalid without a description" do
+      project = build(:project, description: nil)
+
+      expect(project).to be_invalid
+    end
+
+    it "is invalid without a goal" do
+      project = build(:project, goal: nil)
+
+      expect(project).to be_invalid
+    end
+  end
 end
