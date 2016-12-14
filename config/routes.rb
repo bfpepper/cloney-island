185b2491ae2e1e9ff13889336651c0cceedb7f1a
Ben Pepper <bfpepper87@gmail.com>
2016-12-14 13:46:57 -0700

    add landing controller and index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'landing#index'
+
   resources :users, only: [:new, :create, :show]
 
 end
--- spec/features/registered_users/user_can_log_in_spec.rb
+++ spec/features/registered_users/user_can_log_in_spec.rb
@@ -4,21 +4,20 @@ describe "a registered user can log back in" do
   it "an existing user can log back into thier account" do
     user = create(:user)
 
+    # when a registered user visits root
     visit root_path
 
+    # clicks on login
     click_on "Login"
 
+    # enters form credentials
     fill_in :email, with: user.email
     fill_in :password, with: user.password
 
+    # clicks sign in
     click_on "Sign In"
 
+    # redirected to root
     expect(current_page).to eq(root_path)
   end
 end
-
-# when a registered user visits root
-# clicks on login
-# enters form credentials
-# clicks sign in
-# redirected to root
