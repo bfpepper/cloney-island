578dfb1c2f445a5e34a8229a3cb7d30303037f86
Ben Pepper <bfpepper87@gmail.com>
2016-12-14 13:40:36 -0700

    add factory for user.

--- spec/features/registered_users/user_can_log_in_spec.rb
+++ spec/features/registered_users/user_can_log_in_spec.rb
@@ -1,14 +1,21 @@
-# require "rails_helper"
-#
-# describe "a registered user can log back in" do
-#   it "an existing user can log back into thier account" do
-#     user = create(:user)
-#
-#     visit login_path
-#
-#
-#   end
-# end
+require "rails_helper"
+
+describe "a registered user can log back in" do
+  it "an existing user can log back into thier account" do
+    user = create(:user)
+
+    visit root_path
+
+    click_on "Login"
+
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+
+    click_on "Sign In"
+
+    expect(current_page).to eq(root_path)
+  end
+end
 
 # when a registered user visits root
 # clicks on login
