288f4742d05da43ce935b193db9ec4e66dd8dd77
Ben Pepper <bfpepper87@gmail.com>
2016-12-14 13:29:43 -0700

    add user validations with cooesponding tests.

---
+++ spec/features/registered_users/user_can_log_in_spec.rb
@@ -0,0 +1,17 @@
+# require "rails_helper"
+#
+# describe "a registered user can log back in" do
+#   it "an existing user can log back into thier account" do
+#     user = create(:user)
+#
+#     visit login_path
+#
+#
+#   end
+# end
+
+# when a registered user visits root
+# clicks on login
+# enters form credentials
+# clicks sign in
+# redirected to root
