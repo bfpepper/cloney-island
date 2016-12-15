9ad54f8aa916e34afd59e3a9e38d54ec6989fbc7
Lee Saville <l.w.saville@gmail.com>
2016-12-14 17:44:18 -0700

    Add validation testing

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'is valid with name' do
+      category = build(:category)
+
+      expect(category).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      category = build(:category)
+
+      expect(category).to be_valid
+    end
+
+    it 'validate uniqueness' do
+      create(:category, name: "Frank")
+      category = build(:category, name: "Frank")
+
+      expect(category).to be_invalid 
+    end
+  end
 end
