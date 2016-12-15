a87e9db161b24bda9b7af01f0979a9ceb77c25c6
Lee Saville <l.w.saville@gmail.com>
2016-12-14 17:30:31 -0700

    Add create action with category_params

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,21 @@
 class Admin::CategoriesController < ApplicationController
   def new
-    @category = Category.new 
+    @category = Category.new
   end
+
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      flash[:success] = "Successfully created #{@category.name}"
+      redirect_to admin_categories_path(category: @category.slug)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
