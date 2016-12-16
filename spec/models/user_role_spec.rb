402fd513dd1d59e2b88283f867c3fc01d33e5935
antciccone <aciccone@bu.edu>
2016-12-15 17:54:03 -0700

    Add testing for relationships between user and role

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UserRole, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Relationships" do
+    it "Belongs to user" do
+      user_role = build(:user_role)
+
+      expect(user_role).to respond_to(:user)
+    end
+
+    it "Belongs to role" do
+      user_role = build(:user_role)
+
+      expect(user_role).to respond_to(:role)
+    end
+  end
 end
