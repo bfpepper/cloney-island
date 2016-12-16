0f9b50b9cdce93caf4ca0b5958e62d73d1bc0b2e
Ben Pepper <bfpepper87@gmail.com>
2016-12-16 13:59:31 -0700

    add permission_ervice testing.

--- spec/services/permisson_service_spec.rb
+++ spec/services/permisson_service_spec.rb
@@ -3,17 +3,41 @@ require 'rails_helper'
 
 describe 'PermissionService' do
   context '#allow?' do
-    it "guest returns false for admin" do
+    it "registered user returns nil for admin" do
       user = create(:user)
       user.roles << Role.create(name: "registered")
       controller = "admin/categories"
       action = "new"
 
-      guest = PermissionsService.new(user, controller, action)
+      permission = PermissionsService.new(user, controller, action)
 
-      guest.allow?
+      guest = permission.allow?
 
-      expect(guest).to eq(false)
+      expect(guest).to eq(nil)
+    end
+
+    it "guest returns false for admin" do
+      user = create(:user)
+      controller = "admin/categories"
+      action = "new"
+
+      permission = PermissionsService.new(user, controller, action)
+
+      guest = permission.allow?
+
+      expect(guest).to eq(nil)
+    end
+
+    it "guest returns false for registered" do
+      user = create(:user)
+      controller = "projects"
+      action = "new"
+
+      permission = PermissionsService.new(user, controller, action)
+
+      guest = permission.allow?
+
+      expect(guest).to eq(nil)
     end
   end
 end
