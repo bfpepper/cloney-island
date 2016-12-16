98f1efb02d7134ab231f0ef029ddc1fc302b18f9
antciccone <aciccone@bu.edu>
2016-12-15 17:48:40 -0700

    Add relationship test to role

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Relationships' do
+    it 'has many users' do
+      role = build(:role)
+
+      expect(role).to respond_to(:user)
+    end
+  end
 end
