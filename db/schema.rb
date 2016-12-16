f6e71c5054aca73972e539228607871c7cbffd18
antciccone <aciccone@bu.edu>
2016-12-15 17:20:47 -0700

    Create role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215232451) do
+ActiveRecord::Schema.define(version: 20161216000217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20161215232451) do
     t.string   "slug"
     t.integer  "category_id"
     t.index ["category_id"], name: "index_projects_on_category_id", using: :btree
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_projects", force: :cascade do |t|
