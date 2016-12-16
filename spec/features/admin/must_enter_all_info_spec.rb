835b3f6bfbeacd831ff8952e2b8e27add91db78b
antciccone <aciccone@bu.edu>
2016-12-15 19:56:41 -0700

    Refactor admin can create category

--- spec/features/admin/must_enter_all_info_spec.rb
+++ spec/features/admin/must_enter_all_info_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 describe 'An admin can create a category' do
   context 'authorized admin' do
     scenario 'they visit new_category_path' do
-      # Need stub for admin authorization
+
+      admin = create(:user)
+      role = Role.create(name: "admin")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      admin.roles << role
+
       visit new_admin_category_path
 
       click_button "Create Category"
@@ -12,6 +17,12 @@ describe 'An admin can create a category' do
     end
 
     scenario "Admin MUST enter all info to edit a category" do
+      
+      admin = create(:user)
+      role = Role.create(name: "admin")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      admin.roles << role
+
       category = create(:category)
 
       visit admin_edit_category_path(category.slug)
