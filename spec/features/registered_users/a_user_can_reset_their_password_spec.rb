d32a7166d186cf1f54a798f629456e1e72412511
Ben Pepper <bfpepper87@gmail.com>
2016-12-17 13:11:28 -0700

    build test for reset password.

---
+++ spec/features/registered_users/a_user_can_reset_their_password_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe "A user can reset password" do
+  it "User can reset password using Twillo 2FA" do
+
+    user = create(:user)
+
+    visit login_path
+    click_on "Forgot Password?"
+
+    expect(current_path).to eq(pass_reset_path)
+
+    fill_in "Phone", with: "7203230072"
+    click_on "Reset Password"
+
+    expect(current_path).to eq(twillo_path)
+
+    fill_in "NUMBER", with: '<Twillo number>'
+    fill_in "Passowrd", with: "Turing1510"
+    fill_in "Passowrd_confirmation", with: "Turing1510"
+    click_on "Update Password"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "session[email]", with: user.email
+    fill_in "session[password]", with: "Turing1510"
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content("#{user.name}")
+  end
+end
