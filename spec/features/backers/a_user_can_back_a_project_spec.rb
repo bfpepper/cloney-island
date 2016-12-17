fde68fe66594e9bbba3631b15282fc9627188352
Lee Saville <l.w.saville@gmail.com>
2016-12-17 13:59:29 -0700

    Write test around a registered user backing a project

--- spec/features/backers/a_user_can_back_a_project_spec.rb
+++ spec/features/backers/a_user_can_back_a_project_spec.rb
@@ -1,21 +1,39 @@
-# require 'rails_helper'
-#
-# describe "As a user" do
-#  context "when I visit a projects show page" do
-#   scenario "I can back(give money) to a project" do
-#       project = create(:project, name: "How to find a Job")
-#       user = create(:user)
-#       role = Role.create(name: "registered")
-#       user.roles << role
-#
-#       visit project_path(project)
-#       click_on "Back Project"
-#
-#       expect(page).to have_content("How to find a Job")
-#       fill_in :amount with 50
-#
-#       expect(current_path).to eq(something)
-#       #need some more expectations
-#     end
-#   end
-# end
+require 'rails_helper'
+
+describe "As a user" do
+ context "when I visit a projects show page" do
+  scenario "I can back(give money) to a project" do
+      project = create(:project, name: "How to find a Job", goal: 100)
+      user = create(:user)
+      role = Role.create(name: "registered")
+      user.roles << role
+
+      visit project_path(project)
+
+      expect(find('div.progress-bar')['aria-valuenow']).to eq('0')
+
+      click_on "Comments"
+
+      within('div.tab-pane.active') do
+        expect(page).to have_content("Only backers can post comments.")
+      end
+
+      click_on "Back Project"
+
+      expect(current_path).to eq(projects_new_backer_path(project.slug))
+      expect(page).to have_content("How to find a Job")
+
+      fill_in :amount, with: 50
+      click_on "Back Project"
+
+      expect(current_path).to eq(project_path(project.slug))
+      expect(find('div.progress-bar')['aria-valuenow']).to eq('10')
+
+      click_on "comments"
+
+      within('div.tab-pane.active') do
+        expect(page).to have_button("Post Your Comment")
+      end
+    end
+  end
+end
