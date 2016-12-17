050a0b39b43441a31e33ce0c97e31c0ad4250263
antciccone <aciccone@bu.edu>
2016-12-16 22:21:15 -0700

    Add style to show page

--- spec/features/backers/a_user_can_back_a_project_spec.rb
+++ spec/features/backers/a_user_can_back_a_project_spec.rb
@@ -1,24 +1,21 @@
-require 'rails_helper'
-
-describe "As a user" do
- context "when I visit a projects show page" do 
-  scenario "I can back(give money) to a project" do 
-      user = create(:user)
-      role = Role.create(name: "registered")
-      user.roles << role
-  
-    end 
-  end 
-end 
-
-
-
-
-#as a registered user
-#when i visit project show page
-#I can click on "back project"
-#I am directed to project backers page
-#I fill in the amount
-#I fill in passwordword
-#I click on submit project
-#I am redirected to project show page
+# require 'rails_helper'
+#
+# describe "As a user" do
+#  context "when I visit a projects show page" do
+#   scenario "I can back(give money) to a project" do
+#       project = create(:project, name: "How to find a Job")
+#       user = create(:user)
+#       role = Role.create(name: "registered")
+#       user.roles << role
+#
+#       visit project_path(project)
+#       click_on "Back Project"
+#
+#       expect(page).to have_content("How to find a Job")
+#       fill_in :amount with 50
+#
+#       expect(current_path).to eq(something)
+#       #need some more expectations
+#     end
+#   end
+# end
