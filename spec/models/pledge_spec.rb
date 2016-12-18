77193e697d63326bbb6cf20cce6b1df5e1e69c46
Lee Saville <l.w.saville@gmail.com>
2016-12-18 14:46:24 -0700

    Add relationship and validation testing

--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Pledge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Relationships" do
+    it 'belongs to a user' do
+      pledge = build(:pledge)
+
+      expect(pledge).to respond_to(:user)
+    end
+
+    it 'belongs to a project' do
+      pledge = build(:pledge)
+
+      expect(pledge).to respond_to(:project)
+    end
+  end
+
+  context 'validations' do
+    it 'needs an amount' do
+      pledge = build(:pledge, amount_given: nil)
+
+      expect(pledge).to_not be_valid
+    end
+
+    it 'needs a user' do
+      pledge = build(:pledge, user: nil)
+
+      expect(pledge).to_not be_valid
+    end
+
+    it 'needs a project' do
+      pledge = build(:pledge, project: nil)
+
+      expect(pledge).to_not be_valid
+    end
+  end
 end
