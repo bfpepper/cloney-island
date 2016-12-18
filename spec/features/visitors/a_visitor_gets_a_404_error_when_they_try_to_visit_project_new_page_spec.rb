8486b660c25cb9c687592545f6f60d01664674d6
antciccone <aciccone@bu.edu>
2016-12-18 15:28:57 -0700

    visitor gets flash message and redirected to login when they click on start a project

--- spec/features/visitors/a_visitor_gets_a_404_error_when_they_try_to_visit_project_new_page_spec.rb
+++ spec/features/visitors/a_visitor_gets_a_404_error_when_they_try_to_visit_project_new_page_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 describe 'As a guest' do
-  context 'When I try to visit new_project_path' do
+  context 'When I type in project/new in url bar' do
     scenario 'page does not exist' do
       visit new_project_path
 
       expect(page).to have_content("The page you were looking for doesn't exist")
     end
   end
+
+  context 'When a visitor clicks on Start a Project!'
+    scenario 'page does not exist' do
+    end
+  end 
 end
