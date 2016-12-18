db8bee45bae2fc2d8ca034d6d19af2e78d75c3d4
Lee Saville <l.w.saville@gmail.com>
2016-12-17 18:35:09 -0700

    Add content to create action to create the pledge"

--- app/controllers/projects/pledges_controller.rb
+++ app/controllers/projects/pledges_controller.rb
@@ -5,5 +5,22 @@ class Projects::PledgesController < ApplicationController
   end
 
   def create
+    @pledge = Pledge.create(pledge_params)
+    if @pledge.save
+      current_user.roles << Role.find_by(name: "backer")
+      flash[:success] = "You've successfully backed #{@pledge.project.title}!"
+      redirect_to project_path(@pledge.project.slug)
+    else
+      render :new
+    end
   end
+
+  private
+    def pledge_params
+      params.require(:pledge).permit(:amount_given).merge(user_and_project)
+    end
+
+    def user_and_project
+      {user: current_user, project: Project.find_by_slug(params[:slug])}
+    end
 end
