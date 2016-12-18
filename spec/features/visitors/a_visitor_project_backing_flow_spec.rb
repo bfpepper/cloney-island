f77a81ff0ca82609ad007f418cc0ff2fea840ab6
Noah Berman <noah@noahberman.org>
2016-12-17 19:52:46 -0700

    test for visitor having to sign up prior to backing

--- spec/features/visitors/a_visitor_project_backing_flow_spec.rb
+++ spec/features/visitors/a_visitor_project_backing_flow_spec.rb
@@ -10,7 +10,7 @@ describe "guest visits project site" do
       
       expect(page).to have_content("Email")
       expect(page).to have_content("Password")
-      expect(page).to have_content("or Sign Up")
+      expect(page).to have_content("or create an account!")
       # expect(page).to have_content("You need to login or sign up to do that.")
     end
     
@@ -24,12 +24,42 @@ describe "guest visits project site" do
       visit project_path(project.slug)
       click_button "Back project!"
       
-      expect(page).to have_content("or Sign Up")
+      expect(page).to have_content("or create an account!")
       
       fill_in "session[email]", with: user.email
       fill_in "session[password]", with: user.password
 
       click_on "Sign In"
+      
+      visit project_path(project.slug)
+      click_button "Back project!"
+      fill_in :pledge_amount_given, with: 50
+      click_on "Back project!"
+
+      expect(page).to have_button("Post Your Comment")
+    end
+    
+    scenario "I must sign up before I can back a project" do
+      project = create(:project, title: "MooseWatch")
+      registered = Role.create(name: "registered", id: 1)
+      backer = Role.create(name: "backer")
+      
+      visit project_path(project.slug)
+      click_button "Back project!"
+      
+      expect(page).to have_content("or create an account!")
+      
+      click_on "create an account"
+      
+      fill_in 'user[name]', with: "Test Bob 1"
+      fill_in "user[phone]", with: "555-555-5555"
+      fill_in "user[email]", with: "ohno@example.com"
+      fill_in "user[email_confirmation]", with: "ohno@example.com"
+      fill_in "user[password]", with: "hunter42"
+      fill_in "user[password_confirmation]", with: "hunter42"
+
+      click_on 'Create Account'
+      save_and_open_page
       
       visit project_path(project.slug)
       click_button "Back project!"
