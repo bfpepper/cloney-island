78f46ba94713379c61927b9301fa627f9193eea8
antciccone <aciccone@bu.edu>
2016-12-18 14:27:56 -0700

    refactor a user can create a account

--- spec/features/visitors/a_visitor_can_create_an_account_spec.rb
+++ spec/features/visitors/a_visitor_can_create_an_account_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 describe "Creating an Account" do
   scenario "a visitor can create an account" do
-    Role.create(name: 'registered', id: 1)
+    Role.create(name: 'registered')
 
     visit new_user_path
 
-    fill_in 'user[name]', with: "Test Bob"
+    fill_in 'user[name]', with: "test"
     fill_in "user[phone]", with: "303-555-5555"
     fill_in "user[email]", with: "test_email@example.com"
     fill_in "user[email_confirmation]", with: "test_email@example.com"
-    fill_in "user[password]", with: "password"
-    fill_in "user[password_confirmation]", with: "password"
+    fill_in "user[password]", with: "123"
+    fill_in "user[password_confirmation]", with: "123"
 
-    click_on 'Create Account'
-
+    click_on "Create Account"
+    save_and_open_page
     expect(page).to have_content("test_email@example.com")
-    #need to add flash message
   end
 
   scenario "a visitor can create an account" do
@@ -29,7 +28,7 @@ describe "Creating an Account" do
     fill_in "user[password]", with: "password"
     fill_in "user[password_confirmation]", with: "password"
 
-    click_on 'Create Account'
+    click_on "Create Account"
 
     expect(current_path).to eq(new_user_path)
   end
