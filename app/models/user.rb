29757c42f704ad6cd0b8b2cfa08948dd2f221a6c
Noah Berman <noah@noahberman.org>
2016-12-19 16:18:46 -0700

    re-wrote code for user avatar on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,19 @@ class User < ApplicationRecord
   
   has_attached_file :avatar,
                     :storage => :s3,
-                    :s3_credentials => Proc.new{ |a| a.instance.s3_credentials }
+                    :s3_credentials => Proc.new{ |a| a.instance.s3_credentials },
+                    :styles => {
+                                 thumb: '100x100>',
+                                 square: '200x200>',
+                                 medium: '300x300>'
+                                }
+
+ # Validate the attached image is image/jpg, image/png, etc
+ validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
   validates_attachment_content_type :avatar, content_type: /\Aimage/
   # Validate filename
   validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
-  # Explicitly do not validate
 
   validates_confirmation_of :password
 
