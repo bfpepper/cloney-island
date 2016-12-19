2c5a95f192f1f76ccb744702a2791bd1011d0d50
Ben Pepper <bfpepper87@gmail.com>
2016-12-19 13:51:45 -0700

    add model level tests for comments.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    it { should validate_presence_of(:comment_body)}
+  end
+
+  context 'Relationships' do
+    it { should belong_to(:user)}
+    it { should belong_to(:project)}
+  end
 end
