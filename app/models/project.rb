769daa969e848348ea10109bd4143d8e29b6fc5d
Lee Saville <l.w.saville@gmail.com>
2016-12-18 20:23:41 -0700

    Add relationship with comments and commenters

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,8 @@ class Project < ApplicationRecord
             :goal,
             :category_id, presence: true
 
-  belongs_to :category
+  validates :title,
+             :slug, uniqueness: true
 
   has_many :pledges
   has_many :backers, through: :pledges, source: :user
@@ -12,11 +13,10 @@ class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
 
-  validates :title,
-             :slug, uniqueness: true
+  has_many :comments
+  has_many :commenters, through: :comments, source: :user
 
   belongs_to :category
-
 
   def funding_received
     pledges.empty? ? 0 : ((pledges.sum(:amount_given) / goal.to_f) * 100).round
