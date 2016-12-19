1cf992a36d282080b6b60fc4751458675fcf2e95
Noah Berman <noah@noahberman.org>
2016-12-19 14:19:40 -0700

    hotfix for env variable and gitignore

--- lib/message_sender.rb
+++ lib/message_sender.rb
@@ -1,9 +1,9 @@
 module MessageSender
   def self.send_code(phone_number, code)
-    sid = ENV['twillo_sid']
-    token = ENV['twillo_token']
+    sid = ENV['twilio_sid']
+    token = ENV['twilio_token']
     client = Twilio::REST::Client.new(sid, token)
-    from_number = ENV['twillo_number']
+    from_number = ENV['twilio_number']
 
     message = client.messages.create({
       from: from_number,
