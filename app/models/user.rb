94fab1ed50be5623f7d2569ad46d556647c8793d
Noah Berman <noah@noahberman.org>
2016-12-19 16:48:56 -0700

    potentially fixed problem with paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,16 @@ class User < ApplicationRecord
   has_secure_password
   
   has_attached_file :avatar,
-                    :storage => :s3,
-                    :s3_credentials => Proc.new{ |a| a.instance.s3_credentials },
                     :styles => {
                                  thumb: '100x100>',
                                  square: '200x200>',
                                  medium: '300x300>'
                                 }
 
- # Validate the attached image is image/jpg, image/png, etc
- validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  
+ # # Validate the attached image is image/jpg, image/png, etc
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+ #  
   validates_attachment_content_type :avatar, content_type: /\Aimage/
-  # Validate filename
-  validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
 
   validates_confirmation_of :password
 
@@ -47,10 +43,6 @@ class User < ApplicationRecord
     roles.exists?(name: "backer")
   end
   
-  def s3_credentials
-    {:bucket => "vicarious-cloney-bucket", :access_key_id => "#{ENV['AWS_ACCESS_KEY_ID']}", :secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}"}
-  end
-
   before_validation :generate_api_key
 
   def generate_api_key
