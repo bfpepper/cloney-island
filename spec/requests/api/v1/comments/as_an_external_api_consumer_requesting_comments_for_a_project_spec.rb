f849c5fb43bfc02f3497f4c96fe1c8cd7e40ee72
Lee Saville <l.w.saville@gmail.com>
2016-12-18 20:33:09 -0700

    Change expectations for the nesting of resultant json

--- spec/requests/api/v1/comments/as_an_external_api_consumer_requesting_comments_for_a_project_spec.rb
+++ spec/requests/api/v1/comments/as_an_external_api_consumer_requesting_comments_for_a_project_spec.rb
@@ -13,15 +13,13 @@ describe "comments endpoint" do
       get "/api/v1/comments?api_key=#{user.api_key}&project=#{project.title.parameterize}"
 
       comments = JSON.parse(response.body)
-      
+
       expect(response).to be_success
       expect(comments).to be_an(Array)
-      expect(comment.first['commenter']).to eq(commenter1.name)
-      expect(comment.first['project']).to eq(project.title)
-      expect(comment.first['comment_body']).to eq('What a fantastic project!')
-      expect(comment.last['commenter']).to eq(commenter2.name)
-      expect(comment.last['project']).to eq(project.title)
-      expect(comment.last['comment_body']).to eq('I wish you all the best of luck')
+      expect(comments.first['user']['name']).to eq(commenter1.name)
+      expect(comments.first['comment_body']).to eq('What a fantastic project!')
+      expect(comments.last['user']['name']).to eq(commenter2.name)
+      expect(comments.last['comment_body']).to eq('I wish you all the best of luck')
     end
   end
 end
