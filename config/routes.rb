bcc150c821ccbb3eef48f2ac0cf1cfcc8772dcfb
Ben Pepper <bfpepper87@gmail.com>
2016-12-20 14:43:17 -0700

    routes and views for editing user status

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,6 @@ Rails.application.routes.draw do
     get "/categories/:category/edit", to: 'categories#edit', as: :edit_category
     resources :users, only: [:index]
     get "/admin/users/:id/edit", to: 'users#edit', as: :edit_user
-    put "/admin/users/:id/edit", to: 'users#update'
+    put "/admin/users/:id/edit", to: 'admin/users#update'
   end
 end
