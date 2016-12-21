fbd9786ba1eab055bcf11c409d26c5b6b37b6cbd
Ben Pepper <bfpepper87@gmail.com>
2016-12-21 15:10:53 -0700

    chane addUser Controller to if user.

--- app/controllers/users/add_user_controller.rb
+++ app/controllers/users/add_user_controller.rb
@@ -2,7 +2,7 @@ class Users::AddUserController < ApplicationController
 
   def update
     user = User.find_by(email: params[:add_user][:new_owner_email])
-    if User.exists?(user.id)
+    if user
       @project = Project.find_by(slug: params[:add_user][:project_slug])
       @project.users << user
       redirect_to project_path(@project.slug)
