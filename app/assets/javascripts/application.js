7e4dc9a150a71635ec0647a3efb6738f0c23987e
Lee Saville <l.w.saville@gmail.com>
2016-12-20 21:36:10 -0700

    Add ajax call getting the variables from the view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,23 +10,33 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery.timeago
 //= require jquery
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 $(document).ready(function(){
+
+  jQuery("time.timeago").timeago();
+
+
   $('.comment-button').on('click', function(){
     $('.comment-button').addClass('pink');
+
+    var slug = $('.var').data('url');
+    var api_key = $('.var').data('key');
+    var dataParams = { comment: $('#comment-body').val(),
+                       api_key: api_key
+                     }
     $.ajax({
-      url: "http://localhost:8080/api/v1/projects/harry-potter/comments",
-      contentType: "application/json",
-      type: "GET",
-      dataType: "json",
-      headers: { "api_key": "650c51969893f9f6bae6e7589f5741cd"},
-      success: function(data) { alert("ajax worked"); },
-      error: function(data) { debugger; },
-      done: function(data){ alert(data); }
+      type: "POST",
+      url: "http://localhost:3000/api/v1/projects/" + slug + "/comments",
+      data: dataParams,
+      success: function(comment){
+        $('#comment-body').val('');
+        $('.comments-list').prepend("<li class='blah'><div class='comment-main-level'><div class='comment-avatar'><img src='/assets/lee.png-0f3fd6046ca3c50463ffef9685514744d33dc12c74dc8a974427ed63faf27c10.jpg'></div><div class='comment-box'><div class='comment-head'><h6 class='comment-name by-author'>" + comment.user.name + " | " + jQuery.timeago(comment.created_at) + "</h6></div><div class='comment-content'>" + comment.comment_body + "</div></div></div></li>")
+      }
       });
     });
   });
