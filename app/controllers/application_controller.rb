1b7a646ea67af7fbc8fe6fa82953fecdf237a599
Lee Saville <l.w.saville@gmail.com>
2016-12-21 07:23:29 -0700

    Add helper method to application controller for current_user_api_key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :current_user_api_key
 
   before_action :authorize!
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user_api_key
+    current_user.api_key if current_user
   end
 
   def logged_in?
