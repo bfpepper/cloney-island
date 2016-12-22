59d87c94e38fc157368f194144fd7eec8cfc44ae
Lee Saville <l.w.saville@gmail.com>
2016-12-21 17:57:14 -0700

    Add two possibilities for the user avatar so that the ajax response and 'virtual' comment gets displayed with the right image

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,18 @@ class UserSerializer < ActiveModel::Serializer
   attributes :name, :avatar_url
 
   def avatar_url
-    object.avatar.url(:comment)
+    if object.avatar_file_name.nil?
+      no_image
+    else
+      image
+    end
+  end
+
+  def image
+    "https://s3-us-west-2.amazonaws.com/vicarious-cloney-bucket" + object.avatar.url(:comment)
+  end
+
+  def no_image
+    "https://s3-us-west-2.amazonaws.com/vicarious-cloney-bucket/default/missing.png"
   end
 end
